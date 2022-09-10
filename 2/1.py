a045537c2ef1e8ae1d32243ebfbf553401b6902c
Macroden <krasilnikov_d.20@mail.ru>
2022-09-10 14:52:10 +0300

    исправлены ошибки второго задания

--- 2/1.py
+++ 2/1.py
@@ -6,9 +6,9 @@ print(left)
 print(right)
 sum = 0
 while (left != 0):
-    sum = sum + (left % 10)
-    left = left // 10
+    sum += (left % 10)
+    left //= 10
 while (right != 0):
-    sum = sum + (right % 10)
-    right = right // 10
+    sum += (right % 10)
+    right //= 10
 print(f'Сумма всех цифр числа равна {sum}')
--- 2/4.py
+++ 2/4.py
@@ -1,3 +1,12 @@
 N = int(input("Введите N:"))
+M = input("Введите номера элементов через пробел: ").split(' ')
+M = [int(item) for item in M]
 list1 = list(range(-N, N+1))
+i = 0
+res = 1
+while i < len(M):
+    res = (list1[M[i]])*res
+    i += 1
 print(list1)
+print(M)
+print(res)
