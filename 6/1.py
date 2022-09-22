ade05c5ae799c949b1544407da38353bb3168168
DenKras <krasilnikov_d.20@mail.ru>
2022-09-22 18:07:56 +0300

    домашка № 6

--- 6/1.py
+++ 6/1.py
@@ -9,24 +9,3 @@
 nums = [[[True for x in range(2) for y in range(2) for z in range(2) if (not(x or y or z)) == ((not x) and (not y) and (not z))]]]
 print(nums)
 
-# Задайте список из нескольких чисел. Напишите программу,
-# которая найдёт сумму элементов списка, стоящих на нечётной позиции.
-# from random import random
-
-# list1 = []
-# for i in range(int(input("Введите длину списка: "))):
-#     num = int(random() * 100)
-#     list1.append(num)
-# sum = 0
-# for o in range(1, len(list1), 2):
-#     sum += list1[o]
-# print(list1)
-# print(sum)
-
-# from random import random
-
-# list1 = []
-# for i in range(int(input("Введите длину списка: "))):
-#     num = int(random() * 100)
-#     list1.append(num)
-# res = list(filter(lambda x: x*x, list1))
